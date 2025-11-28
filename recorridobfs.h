2c66b1ae3dcee423161785d69b62d81d221511a5
Valeria Zermeño <ojedadiana339@gmail.com>
2025-11-28 00:32:01 -0600

    Implement BFS function in Grafo class

--- recorridobfs.h
+++ recorridobfs.h
@@ -1,1 +1,36 @@
+#ifndef RECORRIDO_BFS_H
+#define RECORRIDO_BFS_H
+
+#include "Grafo.h"
+
+void Grafo::BFS(int inicio) {
+    if (inicio < 0 || inicio >= n) {
+        cout << "Nodo invalido.\n";
+        return;
+    }
+    bool visitado[MAX] = { false };
+    int cola[MAX];
+    int frente = 0, fin = 0;
+
+    cola[fin++] = inicio;
+    visitado[inicio] = true;
+
+    cout << "\nRecorrido BFS desde " << inicio << ":\n";
+
+    while (frente < fin) {
+        int u = cola[frente++];  
+        cout << u << " ";
+
+        for (int v = 0; v < n; v++) {
+            if (adj[u][v] == 1 && !visitado[v]) {
+                visitado[v] = true;
+                cola[fin++] = v;
+            }
+        }
+    }
+
+    cout << "\n";
+}
+
+#endif
 
