4bf8bef3acbe1119d3117ebb3b82ff2257c86761
V Suarez <vladyasuave@gmail.com>
2025-12-02 15:15:00 -0600

    Lógica inicial para manejo de tablas Hash

---
+++ TablasHash.h
@@ -0,0 +1,122 @@
+#ifndef TABLAS_HASH
+#define TABLAS_HASH
+#include <iostream>
+#include <vector>
+#include <list>
+#include <string>
+using namespace std;
+
+/* MANEJO DE TABLA HASH DE LA RED DE NODOS Y ARISTAS */
+
+struct Nodo{
+    int id;
+    string nombre;
+
+    Nodo(int i = -1, const string& nom = "")
+        : id(i), nombre(nom) {}
+};
+
+struct Arista{
+    int origen;
+    int destino;
+    float peso;
+};
+
+class HashRed{
+    private:
+        vector<list<Nodo>> tabla;
+        size_t capacidad;
+
+        size_t hashID(int id) const{
+            return id % capacidad;
+        }
+    public:
+        HashRed(size_t cap = 100) : tabla(cap), capacidad(cap) {}
+
+        void insertar(const Nodo& n){
+            size_t idx = hashID(n.id);
+
+            for(auto& nd: tabla[idx]){
+                if(nd.id == n.id){
+                    nd = n;
+                    return;
+                }
+            }
+            tabla[idx].push_back(n);
+        }
+
+        Nodo* buscar(const int& id){
+            size_t idx = hashID(id);
+            for(auto& nd: tabla[idx]){
+                if(nd.id == id) return &nd;
+            }
+            return nullptr;
+        }
+
+        void imprimirDebug() const{
+            for(size_t i=0; i<capacidad; ++i){
+                cout << "\n\t Bucket " << i << ": ";
+                for(const auto& nd : tabla [i]){
+                    cout << "[ " << nd.id << " | " << nd.nombre << " ]";
+                }
+                cout << "\n";
+            }
+        }
+
+};
+
+/* MANEJO DE TABLA HASH DE VEHICULOS */
+struct Vehiculo{
+    string placa;
+    string tipo;
+    int id;
+    int destino;
+    size_t horaEntrada;
+
+    Vehiculo(string p = "", string t = "", int i = -1, int d = -1, size_t hE= 0.0)
+        : placa(p), tipo(t), id(i), destino(d), horaEntrada(hE) {}
+};
+
+class HashVehiculos{
+    private:
+        vector<list<Vehiculo>> tabla;
+        size_t capacidad;
+
+        size_t hashID(int id) const{
+            return id % capacidad;
+        }
+    public:
+        HashVehiculos(size_t cap = 100) : tabla(cap), capacidad(cap) {}
+        
+        void insertar(const Vehiculo& v){
+            size_t idx = hashID(v.id);
+
+            for(auto& vh: tabla[idx]){
+                if(vh.id == v.id){
+                    vh = v;
+                    return;
+                }
+            }
+            tabla[idx].push_back(v);
+        }
+
+        Vehiculo* buscar(const int& id){
+            size_t idx = hashID(id);
+            for(auto& vh: tabla[idx]){
+                if(vh.id == id) return &vh;
+            }
+            return nullptr;
+        }
+
+        void imprimirDebug() const{
+            for(size_t i=0; i<capacidad; ++i){
+                cout << "\n\t Bucket " << i << ": ";
+                for(const auto& nd : tabla [i]){
+                    cout << "[ " << nd.id << " | " << " ]";
+                }
+                cout << "\n";
+            }
+        }
+};
+
+#endif
---
+++ leerCSV.h
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include "TablasHash.h"
+using namespace std;
+
+/* LEER ARCHIVO .CSV DE RED DE NODOS*/
+void csvRed(const string& archivo, HashRed& hashRed, vector<Arista>& aristas){
+    fstream red(archivo);
+    if(!red){
+        cout << "\n\t Error al abrir el archivo";
+        return;
+    }
+
+    string linea;
+    while(getline(red, linea)){
+        if(linea.empty()) continue;
+        if(linea[0] == '#') continue;
+
+        stringstream ss(linea);
+        string tipo;
+        getline(ss, tipo, ';'); //Obtiene el tipo de dato que se maneja N o E
+
+        if(tipo == "N"){
+            string stringId, nombre;
+            getline(ss, stringId, ';');
+            getline(ss, nombre, ';');
+
+            int id = stoi(stringId);
+            hashRed.insertar(Nodo(id, nombre));
+        }else if(tipo == "E"){
+            string sOrigen, sDestino, sPeso;
+
+            getline(ss, sOrigen, ';');
+            getline(ss, sDestino, ';');
+            getline(ss, sPeso, ';');
+
+            Arista a;
+            a.origen = stoi(sOrigen);
+            a.destino = stoi(sDestino);
+            a.peso = stoi(sPeso);
+
+            aristas.push_back(a);
+        }
+    }
+}
+
+/* LEER ARCHIVO .CSV DE VEHICULOS */
+
+void csvVehiculos(const string& archivo, HashVehiculos& hashVe, vector<Vehiculo>& vehiculos){
+    fstream vehic(archivo);
+    if(!vehic){
+        cout << "\n\t Error al abrir el archivo";
+        return;
+    }
+
+    string linea;
+    while(getline(vehic, linea)){
+        if(linea.empty()) continue;
+        if(linea[0] == '#') continue;
+
+        stringstream ss(linea);
+        string tipo;
+        getline(ss, tipo, ';'); // Saca la V del principio para que no errores
+
+        string stringId, nombre;
+        getline(ss, stringId, ';');
+        getline(ss, nombre, ';');
+
+        string sPlaca, sTipo, sId, sDestino, sHoraEnt;
+
+            getline(ss, sPlaca, ';');
+            getline(ss, sTipo, ';');
+            getline(ss, sId, ';');
+            getline(ss, sDestino, ';');
+            getline(ss, sHoraEnt, ';');
+
+            Vehiculo v;
+            v.placa = stoi(sPlaca);
+            v.tipo = stoi(sTipo);
+            v.id = stoi(sId);
+            v.destino = stoi(sDestino);
+            v.horaEntrada = stoi(sHoraEnt);
+            hashVe.insertar(Vehiculo(v));
+    }
+}
